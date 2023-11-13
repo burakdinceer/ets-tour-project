2d86cafa4827ee05107125dc4ed532f98c8a5d75
Burak Dinçer <burakdinceer@icloud.com>
2023-11-13 21:16:47 +0300

    header comp

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,8 +1,8 @@
-import { Col, Row } from "antd";
+import { Button, Col, Row } from "antd";
 import React from "react";
 import "./header.scss";
-import { DownOutlined } from '@ant-design/icons';
-import { Dropdown, Space } from 'antd';
+import { BellOutlined, DownOutlined, PhoneOutlined, UserOutlined } from "@ant-design/icons";
+import { Dropdown, Space } from "antd";
 
 const items = [
   {
@@ -24,49 +24,104 @@ const Header = () => {
     <div className="header-comp">
       <Row>
         <Col span={15}>
+          <div className="left-header">
+            <Row>
+              <Col span={5}>
+                <a>
+                  <img style={{ width: "200px" }} src="ets-logo.webp" />
+                </a>
+              </Col>
+              <Col span={2}>
+                <Dropdown
+                  menu={{
+                    items,
+                  }}
+                >
+                  <a
+                    style={{ color: "black" }}
+                    onClick={(e) => e.preventDefault()}
+                  >
+                    <Space>
+                      OTEL
+                      <DownOutlined />
+                    </Space>
+                  </a>
+                </Dropdown>
+              </Col>
+              <Col span={2}>KIBRIS</Col>
+              <Col span={2}>
+                <Dropdown
+                  menu={{
+                    items,
+                  }}
+                >
+                  <a
+                    style={{ color: "black" }}
+                    onClick={(e) => e.preventDefault()}
+                  >
+                    <Space>
+                      TUR
+                      <DownOutlined />
+                    </Space>
+                  </a>
+                </Dropdown>
+              </Col>
+              <Col span={2}>VİLLA</Col>
+              <Col span={2}>UÇAK</Col>
+              <Col span={4}>ERKEN REZERVASYON</Col>
+              <Col span={2}>
+                <a>
+                  <img src="ets-plus.svg" />
+                </a>
+              </Col>
+            </Row>
+          </div>
+        </Col>
+        <Col span={9}>
           <Row>
-            <Col span={5}>
-              <a>
-                <img style={{ width: "200px" }} src="ets-logo.webp" />
-              </a>
-            </Col>
-            <Col span={2}>
+            <Col span={4}>
+              {" "}
               <Dropdown
                 menu={{
                   items,
                 }}
               >
-                <a style={{color:'black'}} onClick={(e) => e.preventDefault()}>
+                <a
+                  style={{ color: "black" }}
+                  onClick={(e) => e.preventDefault()}
+                >
                   <Space>
-                    OTEL
+                    YARDIM
                     <DownOutlined />
                   </Space>
                 </a>
               </Dropdown>
             </Col>
-            <Col span={2}>KIBRIS</Col>
-            <Col span={2}>
-            <Dropdown
-                menu={{
-                  items,
-                }}
-              >
-                <a style={{color:'black'}} onClick={(e) => e.preventDefault()}>
-                  <Space>
-                    TUR
-                    <DownOutlined />
-                  </Space>
-                </a>
-              </Dropdown>
+            <Col span={4}>ACENTELER</Col>
+            <Col span={7}>
+              <div className="call">
+                <PhoneOutlined />
+                <div className="call-text">
+                  <span>4441444</span>
+                  <span>Sizi Arayalım</span>
+                </div>
+              </div>
             </Col>
-            <Col span={2}>VİLLA</Col>
-            <Col span={2}>UÇAK</Col>
-            <Col span={5}>ERKEN REZERVASYON</Col>
-            <Col span={2}><a><img src="ets-plus.svg"/></a></Col>
+
+            <Col span={9}>
+              <div className="user-login">
+                {" "}
+                <div>
+                  <Button>
+                  <UserOutlined />
+                  <span>Giriş Yap</span>
+                  <BellOutlined />
+                  </Button>
+                  
+                </div>
+              </div>
+            </Col>
           </Row>
-        </Col>
-        <Col style={{ background: "blue" }} span={9}>
-          aa
         </Col>
       </Row>
     </div>
